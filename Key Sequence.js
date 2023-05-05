691be9e4b624d364168eb6e33035cdd0b7f9ca47
Facundo Arimany <arimanyfacundo@gmail.com>
2023-05-05 09:46:55 +0200

    Corrected

--- Key Sequence.js
+++ Key Sequence.js
@@ -6,8 +6,7 @@ window.addEventListener('keyup',(e)=>{
   pressed.push(e.key)
   pressed.splice(-secretCode.length -1,pressed.length-secretCode.length)
   console.log(pressed)
-  if(pressed.join().includes(secretCode)){
+  if(pressed.join('').includes(secretCode)){
     console.log('Congrats!!!')
   }
-  
 })
